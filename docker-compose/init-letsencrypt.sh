d41de70fe598420e806f7613d8242090d411b347
Radek Burget <burgetr@fit.vutbr.cz>
2021-09-07 13:16:38 +0200

    certbot config update

--- docker-compose/init-letsencrypt.sh
+++ docker-compose/init-letsencrypt.sh
@@ -32,7 +32,7 @@ fi
 domains=(layout.fit.vutbr.cz)
 rsa_key_size=4096
 data_path="./FrontServer/certbot"
-email="" # Adding a valid address is strongly recommended
+email="burgetr@fit.vutbr.cz" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
