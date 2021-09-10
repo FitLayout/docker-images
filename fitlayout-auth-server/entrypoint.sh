662e2441a098558fbc9c99e55c07b9c8d9f10cd8
Skulaurun Mrusal <Skulaurun@seznam.cz>
2021-09-10 13:33:36 +0200

    Implement public & private key generation and config injection

---
+++ fitlayout-auth-server/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+KEYDIR=/opt/jwt-keys
+PUBDIR=/opt/shared
+
+if [ ! -d "$KEYDIR" ]; then
+    mkdir $KEYDIR
+    openssl genrsa -out $KEYDIR/baseKey.pem
+    openssl pkcs8 -topk8 -inform PEM -in $KEYDIR/baseKey.pem -out $KEYDIR/privateKey.pem -nocrypt
+    openssl rsa -in $KEYDIR/baseKey.pem -pubout -outform PEM -out $KEYDIR/publicKey.pem
+fi
+
+[ -d "$PUBDIR" ] && cp $KEYDIR/publicKey.pem $PUBDIR/publicKey.pem
+
+exec java -XX:MaxRAMPercentage=${MEM_MAX_RAM_PERCENTAGE} -Xss${MEM_XSS} -XX:+UseContainerSupport ${JVM_ARGS} -jar payara-micro.jar "$@"
