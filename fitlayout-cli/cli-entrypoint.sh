7c37a75543df7e8fcc2f750f9e7be289ded5e41e
Radek Burget <burgetr@fit.vutbr.cz>
2022-02-07 12:44:20 +0100

    CLI image configurable from an external config file

--- fitlayout-cli/cli-entrypoint.sh
+++ fitlayout-cli/cli-entrypoint.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 
+# Default config file contained in the image
+DEF_CONF="${CLI_DIR}/config_defaults.properties"
+# Optional user properties mounted from outside
+OPT_CONF="/opt/config/config.properties"
+# Final properties used for the server
+RUN_CONF="${CLI_DIR}/config.properties"
+
+if [ -f $OPT_CONF ]; then
+    cat $DEF_CONF $OPT_CONF >$RUN_CONF
+else
+    cat $DEF_CONF >$RUN_CONF
+fi
+
 exec java -jar "$CLI" "$@"
