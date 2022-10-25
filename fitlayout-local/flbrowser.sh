02e19dea914cb4b0bea6a431e13b6f9380f8bb79
Radek Burget <burgetr@fit.vutbr.cz>
2022-10-25 11:02:27 +0200

    Local browser fixes

--- fitlayout-local/flbrowser.sh
+++ fitlayout-local/flbrowser.sh
@@ -6,6 +6,10 @@ STORAGE_PATH="$HOME/.fitlayout/storage"
 # EDIT: An optional folder with browser extensions to be used for rendering
 EXT_FOLDER="/opt/config/browser-extensions"
 
+# EDIT: Local port used connecting the browser
+PORT="9000"
+
+
 if [ ! $# -eq 0 ]; then 
     STORAGE_PATH="$1"
 fi
@@ -23,10 +27,10 @@ fi
 
 echo "Browsing artifact storage in $STORAGE_PATH";
 echo "Starting the local server. After the server is started please point your server to"
-echo "http://localhost:8099"
+echo "http://localhost:$PORT"
 
 # Run the image
 docker run --rm \
-  -p 127.0.0.1:8099:80/tcp \
+  -p 127.0.0.1:$PORT:80/tcp \
   --mount type=bind,source="$STORAGE_PATH",target=/opt/storage \
   fitlayout/fitlayout-local 2>&1 | grep 'ready in'
