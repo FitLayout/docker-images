6bc61a5874434a4286afd98a91f64494a8783683
Radek Burget <burgetr@fit.vutbr.cz>
2023-05-17 12:23:51 +0200

    AuthGUI build fix

--- fitlayout-auth-gui/build.sh
+++ fitlayout-auth-gui/build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 mkdir app
-cp ../AuthGUI/package*.json ../AuthGUI/*.js ../AuthGUI/.eslintrc.js app
+cp ../AuthGUI/package*.json ../AuthGUI/*.js ../AuthGUI/.eslintrc.cjs ../AuthGUI/index.html app
 cp -r ../AuthGUI/src app
 cp -r ../AuthGUI/public app
 
