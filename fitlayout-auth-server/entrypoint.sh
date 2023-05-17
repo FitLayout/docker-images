fbe5f3decea75168cea42a450692dae93d0b32c5
Radek Burget <burgetr@fit.vutbr.cz>
2023-05-17 15:57:18 +0200

    Key distribution fix

--- fitlayout-auth-server/entrypoint.sh
+++ fitlayout-auth-server/entrypoint.sh
@@ -6,11 +6,12 @@ PUBDIR=/opt/shared
 
 if [ ! -d "$KEYDIR" ]; then
     mkdir $KEYDIR
+    echo "Re-generating keys in $KEYDIR"
     openssl genrsa -out $KEYDIR/baseKey.pem
     openssl pkcs8 -topk8 -inform PEM -in $KEYDIR/baseKey.pem -out $KEYDIR/privateKey.pem -nocrypt
     openssl rsa -in $KEYDIR/baseKey.pem -pubout -outform PEM -out $KEYDIR/publicKey.pem
 fi
 
-[ -d "$PUBDIR" ] && cp $KEYDIR/publicKey.pem $PUBDIR/publicKey.pem
+[ -d "$PUBDIR" ] && cp "$KEYDIR/publicKey.pem" "$PUBDIR/"
 
 exec java -XX:MaxRAMPercentage=${MEM_MAX_RAM_PERCENTAGE} -Xss${MEM_XSS} -XX:+UseContainerSupport ${JVM_ARGS} -jar payara-micro.jar "$@"
