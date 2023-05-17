418593218c1f3a953764b2d0e135b3395cd03823
Radek Burget <burgetr@fit.vutbr.cz>
2023-05-17 11:01:45 +0200

    Frontend build fix

--- fitlayout-frontend/build.sh
+++ fitlayout-frontend/build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 mkdir app
-cp ../PageView/package*.json ../PageView/*.js ../PageView/.eslintrc.js app
+cp ../PageView/package*.json ../PageView/*.js ../PageView/.eslintrc.cjs ../PageView/index.html app
 cp -r ../PageView/src app
 cp -r ../PageView/public app
 
